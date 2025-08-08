94c3b337ed4696a9726a42f9027bce6fa18022b3
David Alsh <12656294+alshdavid@users.noreply.github.com>
2025-08-09 09:52:34 +1000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,4 +2,7 @@ $env:PATH = ";$PSScriptRoot\python;" + $env:PATH
 $env:PATH = ";$PSScriptRoot\python\Scripts;" + $env:PATH
 $env:PATH = ";$PSScriptRoot\zluda;" + $env:PATH
 
+# Optional: Uncomment this if you have an onboard GPU
+# $env:HIP_VISIBLE_DEVICES=1
+
 zluda -- python.exe .\main.py @args
