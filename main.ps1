6ce805107b0eaac383e06a10dc7892816f0a017b
David Alsh <12656294+alshdavid@users.noreply.github.com>
2025-08-08 11:41:31 +1000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -2,7 +2,4 @@ $env:PATH = ";$PSScriptRoot\python;" + $env:PATH
 $env:PATH = ";$PSScriptRoot\python\Scripts;" + $env:PATH
 $env:PATH = ";$PSScriptRoot\zluda;" + $env:PATH
 
-# Optional: Ignore onboard GPU
-$env:HIP_VISIBLE_DEVICES=1
-
-zluda -- python.exe .\main.py --disable-auto-launch --use-quad-cross-attention --reserve-vram 0.9 
+zluda -- python.exe .\main.py @args
